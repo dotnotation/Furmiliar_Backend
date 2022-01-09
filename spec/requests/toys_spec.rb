2963b2dcbf432548f051f0c2c8505ad81694b42d
dotnotation <dotinmotionproductions@gmail.com>
2022-01-09 18:07:37 -0500

    request spec for get and post of toy controller

--- spec/requests/toys_spec.rb
+++ spec/requests/toys_spec.rb
@@ -6,12 +6,32 @@ RSpec.describe "toys", :type => :request do
             toybox = ToyBox.create(name: "Some")
             toybox.toys.create(name: "Simple Name")
             toybox.toys.create(name: "Simple Name2")
-            
+
             get "/toys"
 
             parsed_body = JSON.parse(response.body)
             expect(parsed_body.length).to eq(2)
 
+        end
+    end
+
+    describe "#create" do
+        it "creates a new toy" do
+            # test that database has nothing
+            # create the data
+            # send data to create method
+            # test that database has a toy
+            toy_name = "New Toy"
+
+            expect(Toy.all.length).to eq(0)
+
+            toybox = ToyBox.create(name: "New ToyBox")
+
+            post "/toys", :params => { :toy => {name: toy_name, toy_box_id: toybox.id} }
+
+            parsed_body = JSON.parse(response.body)
+            
+            expect(parsed_body["name"]).to eq(toy_name)
         end
     end
 end
